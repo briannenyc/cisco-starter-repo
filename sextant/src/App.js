ce293bf86757cf1e5ec9abf63c27e4c623ab121c
Brianne Nunnelley <brianne.nyc@mac.com>
2022-07-18 10:28:18 -0500

    added exhibit framework

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Banner from "./components/Banner"
+import Exhibit from "./components/Exhibit"
 
 function App() {
   return (
     <div className="App">
       <Banner></Banner>
+      <Exhibit></Exhibit>
      <h1>Hello World</h1>
     </div>
   );
---
+++ sextant/src/components/Exhibit.js
@@ -0,0 +1,23 @@
+//Create an exhibit component that displays a heading and any child components. 
+// This component will wrap the components we create in the following tasks.
+// Think of it as a pretty container to logically separate different data points.
+
+
+import React from 'react';
+
+const Exhibit = () => {
+
+return (
+    <header>IP addresses
+        <p>any additional info</p>
+    </header>
+
+
+)}
+
+
+
+
+
+
+export default Exhibit;
