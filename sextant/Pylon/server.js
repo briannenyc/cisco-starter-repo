9a27ef9f14bb457baac044b660c448c6c6dd5e2e
Brianne Nunnelley <brianne.nyc@mac.com>
2022-07-25 14:34:28 -0500

    Server is up

--- sextant/Pylon/server.js
+++ sextant/Pylon/server.js
@@ -5,3 +5,17 @@ var WebSocketRouter = require('websocket').router;
 var W3CWebSocket = require('websocket').w3cwebsocket;
 
 console.log("hello world")
+
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    console.log('/ route requested');
+    res.send("hello world")
+})
+
+
+app.listen(55455, () => {
+    console.log('server is up')
+})
