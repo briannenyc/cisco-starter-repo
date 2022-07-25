d5674468dc5f392051b8cad61b760a4962965de5
Brianne Nunnelley <brianne.nyc@mac.com>
2022-07-25 14:19:29 -0500

    added Pylon server

---
+++ sextant/Pylon/server.js
@@ -0,0 +1,7 @@
+var WebSocketServer = require('websocket').server;
+var WebSocketClient = require('websocket').client;
+var WebSocketFrame  = require('websocket').frame;
+var WebSocketRouter = require('websocket').router;
+var W3CWebSocket = require('websocket').w3cwebsocket;
+
+console.log("hello world")
